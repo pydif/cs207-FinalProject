b5cb52af00077558db05c90661041be0408f9dbf
Hamish Nicholson <hamish_nicholson@college.harvard.edu>
2018-11-06 00:17:41 -0500

    fixed syntax errors in elementary.py and wrong tests in tests_elementary.py

--- code/pydif/pydif/elementary.py
+++ code/pydif/pydif/elementary.py
@@ -7,54 +7,54 @@ from dual import Dual
 import numpy as np 
 
 def cos(x):
-	try: 
-		return Dual(np.cos(x), -1 * x.der * np.sin(x))
-	except:
-		return np.cos(x)
+    try: 
+        return Dual(np.cos(x.val), -1 * x.der * np.sin(x.val))
+    except:
+        return np.cos(x)
 
 def sin(x):
-	try: 
-		return Dual(np.sin(x),  x.der * np.cos(x))
-	except:
-		return np.sin(x)
+    try: 
+        return Dual(np.sin(x.val),  x.der * np.cos(x.val))
+    except:
+        return np.sin(x)
 
 def tan(x):
-	try:
-		return Dual(np.tan(x), x.der * (1/np.cos(x))**2)
-	except:
-		return np.tan(x)
+    try:
+        return Dual(np.tan(x.val), x.der * (1/np.cos(x.val))**2)
+    except:
+        return np.tan(x)
 
 def exp(x):
-	try:
-		return Dual(np.exp(x), x.der * np.exp(x))
-	except:
-		return np.exp(x)
+    try:
+        return Dual(np.exp(x.val), x.der * np.exp(x.val))
+    except:
+        return np.exp(x)
 
 def exp2(x):
-	try:
-		return Dual(np.exp2(x), np.exp2(x.val) * (x.der * np.log(2)))
-	except:
-		return np.exp2(x)
+    try:
+        return Dual(np.exp2(x.val), np.exp2(x.val) * (x.der * np.log(2)))
+    except:
+        return np.exp2(x)
 
 # natural log 
 def log(x):
-	try:
-		return Dual(np.log(x), 1/x.val * x.der )
-	except:
-		return np.exp(x)	
+    try: 
+        return Dual(np.log(x.val), (1 / float(x.val)) * x.der  )
+    except:
+        return np.log(x)    
 
 # log base 2
 def log2(x):
-	try:
-		return Dual(np.log2(x), 1/(x.val * np.log(2)) * x.der)
-	except:
-		return np.log2(x)
+    try:
+        return Dual(np.log2(x.val), 1/(x.val * np.log(2)) * x.der)
+    except:
+        return np.log2(x)
 
 # log base 10
 def log10(x):
-	try:
-		return Dual(np.log10(x), 1/(x.val * np.log(10)) * x.der)
-	except:
-		return np.log10(x)
+    try:
+        return Dual(np.log10(x.val), 1/(x.val * np.log(10)) * x.der)
+    except:
+        return np.log10(x)
 
 
--- code/pydif/pydif/test_elementary.py
+++ code/pydif/pydif/test_elementary.py
@@ -1,62 +1,62 @@
 import elementary as el
 import pytest
-from dual import dual 
+from Dual import Dual 
 
-def test_cos();
-	assert(el.cos(5) == pytest.approx(0.2836, 0.001))
-	x = Dual(5, 8)
-	sinx = el.cos(d)
-	assert(sinx.val == pytest.approx(0.2836, 0.001)) 
-	assert(sinx.der == pytest.approx(7.671, 0.001))
+def test_cos():
+    assert(el.cos(5) == pytest.approx(0.2836, 0.001))
+    x = Dual(5, 8)
+    cosx = el.cos(x)
+    assert(cosx.val == pytest.approx(0.2836, 0.001)) 
+    assert(cosx.der == pytest.approx(7.671, 0.001))
 
 def test_sin():
-	assert(el.sin(3) == pytest.approx(0.1411, 0.001))
-	x = Dual(3, 3)
-	cosx = el.cos(x)
-	assert(cosx.val == pytest.approx(0.1411, 0.001)) 
-	assert(cosx.der == pytest.approx(-2.9699, 0.001))
+    assert(el.sin(3) == pytest.approx(0.1411, 0.001))
+    x = Dual(3, 3)
+    sinx = el.sin(x)
+    assert(sinx.val == pytest.approx(0.1411, 0.001)) 
+    assert(sinx.der == pytest.approx(-2.9699, 0.001))
 
 def test_tan():
-	assert(el.tan(2) == pytest.approx(-2.185, 0.001))
-	x = Dual(1, 3)
-	tanx = el.tan(x)
-	assert(tanx.val == pytest.approx(-2.185, 0.001))
-	assert(tanx.der == pytest.approx(10.2765565, 0.001))
+    assert(el.tan(2) == pytest.approx(-2.185, 0.001))
+    x = Dual(2, 3)
+    tanx = el.tan(x)
+    assert(tanx.val == pytest.approx(-2.185, 0.001))
+    assert(tanx.der == pytest.approx(17.32319, 0.001))
 
 def test_exp():
-	assert(el.exp(0) == pytest.approx(0))
-	assert(el.exp(5)) == pytest.approx(148.413159, 0.0001)
-	x = Dual(3, 4)
-	ex = el.exp(x)
-	assert(ex.val == pytest.approx(20.0855369, 0.0001))
-	assert(ex.der = pytest.approx(80.3421476, 0.0001))
+    assert(el.exp(0) == pytest.approx(1))
+    assert(el.exp(5)) == pytest.approx(148.413159, 0.0001)
+    x = Dual(3, 4)
+    ex = el.exp(x)
+    assert(ex.val == pytest.approx(20.0855369, 0.0001))
+    assert(ex.der == pytest.approx(80.3421476, 0.0001))
 
 
 def test_exp2():
-	assert(el.exp2(0) == pytest.approx(0))
-	assert(el.exp2(4) == pytest.approx(16))
-	x = Duel(4, 4)
-	2x = el.exp2(x)
-	assert(2x.val == pytest.approx(16))
-	assert(2x.der == pytest.approx(44.361, 0.001))
+    assert(el.exp2(0) == pytest.approx(1))
+    assert(el.exp2(4) == pytest.approx(16))
+    x = Dual(4, 4)
+    x2 = el.exp2(x)
+    assert(x2.val == pytest.approx(16))
+    assert(x2.der == pytest.approx(44.361, 0.001))
 
 def test_log():
-	assert(el.log(27) == pytest.approx(3.2958, 0.0001))
-	x = Duel(3, 5)
-	lnx = el.log(x)
-	assert(lnx.val == pytest.approx(1.098, 0.001))
-	assert(lnx.der == pytest.approx(4.551, 0.001))
+    assert(el.log(27) == pytest.approx(3.2958, 0.001))
+    x = Dual(3, 5)
+    lnx = el.log(x)
+    assert(lnx.val == pytest.approx(1.098, 0.001))
+    assert(lnx.der == pytest.approx(1.6666, 0.001))
 
 def test_log2():
-	assert(el.log2(8) == pytest.approx(3))
-	x = Duel(4, 8)
-	log2x = el.log2(x)
-	assert(log2x.val == pytest.approx(2))
-	assert(log2x.der == pytest.approx(2.8853, 0.001))
+    assert(el.log2(8) == pytest.approx(3))
+    x = Dual(4, 8)
+    log2x = el.log2(x)
+    assert(log2x.val == pytest.approx(2))
+    assert(log2x.der == pytest.approx(2.8853, 0.001))
 
 def test_log10():
-	assert(el.log10(100) == pytest.approx(10))
-	x = Duel(10, 30)
-	log10x = el.log10(x)
-	assert(log10x.val == pytest.approx(1))
-	assert(log10x.der == pytest.approx(1.30288))
+    assert(el.log10(100) == pytest.approx(2))
+    x = Dual(10, 30)
+    log10x = el.log10(x)
+    assert(log10x.val == pytest.approx(1))
+    assert(log10x.der == pytest.approx(1.30288, 0.001))
