2427ce8c6521c629569de5160c20040d010b7f57
Nick Pagel <pagel@Nicks-MacBook-Pro.local>
2018-11-07 20:09:23 -0500

    duals complete

--- code/pydif/pydif/dual.py
+++ code/pydif/pydif/dual.py
@@ -5,34 +5,54 @@ class Dual():
         self.der = der
 
     def __add__(self, x):
-        self.val +=  x.val
-        self.der +=  x.der
+        try:
+            self.val +=  x.val
+            self.der +=  x.der
+        except AttributeError:
+            self.val += x
+            self.der += x
         return self
 
     def __sub__(self, x):
-        self.val -=  x.val
-        self.der -=  x.der
+        try:
+            self.val -=  x.val
+            self.der -=  x.der
+        except AttributeError:
+            self.val -= x
+            self.der -= x
         return self
 
     def __radd__(self, x):
         return self.__add__(x)
 
     def __mul__(self, x):
-        self.val *= x.val
-        self.der = self.der* x.val + self.val * x.der
+        try:
+            self.der = self.der * x.val + self.val * x.der
+            self.val *= x.val
+        except AttributeError:
+            self.val *= x
+            self.der *= x
         return self
 
     def __rmul__(self, x):
         return self.__mul__(x)
 
     def __truediv__(self, x):
-        self.val /= x.val
-        self.der = (self.der* x.val - self.val * x.der)/(x.val)**2
+        try:
+            self.der = (self.der* x.val - self.val * x.der)/(x.val)**2
+            self.val /= x.val
+        except AttributeError:
+            self.val /= x
+            self.der /= x
         return self
 
     def __pow__(self, x):
-        self.val = self.val**x.val
-        self.der = x.der*self.val**x.val
+        try:
+            self.der = x.der*self.val**x.val
+            self.val = self.val**x.val
+        except AttributeError:
+            self.val = self.val**x
+            self.der = self.der**x
         return self
 
     def __repr__(self):
