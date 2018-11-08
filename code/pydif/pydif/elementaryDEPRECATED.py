affd59919686391cdbb7d2b6303844ae4411f2d4
potato_farmer <WildTangles@gmail.com>
2018-11-08 14:03:02 -0500

    refractored pydif, added deprecated dual,elementary for testing, added gitignore for .pyc

---
+++ code/pydif/pydif/elementaryDEPRECATED.py
@@ -0,0 +1,61 @@
+"""elementary.py
+This file overloads elementary functions which do no have dunder methods
+including exponential and trig functions. The functions first try to work 
+with x as a dual number and falls back to treating x as a normal numerical type"""
+
+#from .dual import Dual 
+from dualDEPRECATED import Dual
+import numpy as np 
+
+def cos(x):
+    try: 
+        return Dual(np.cos(x.val), -1 * x.der * np.sin(x.val))
+    except:
+        return np.cos(x)
+
+def sin(x):
+    try: 
+        return Dual(np.sin(x.val),  x.der * np.cos(x.val))
+    except:
+        return np.sin(x)
+
+def tan(x):
+    try:
+        return Dual(np.tan(x.val), x.der * (1/np.cos(x.val))**2)
+    except:
+        return np.tan(x)
+
+def exp(x):
+    try:
+        return Dual(np.exp(x.val), x.der * np.exp(x.val))
+    except:
+        return np.exp(x)
+
+def exp2(x):
+    try:
+        return Dual(np.exp2(x.val), np.exp2(x.val) * (x.der * np.log(2)))
+    except:
+        return np.exp2(x)
+
+# natural log 
+def log(x):
+    try: 
+        return Dual(np.log(x.val), (1 / float(x.val)) * x.der  )
+    except:
+        return np.log(x)    
+
+# log base 2
+def log2(x):
+    try:
+        return Dual(np.log2(x.val), 1/(x.val * np.log(2)) * x.der)
+    except:
+        return np.log2(x)
+
+# log base 10
+def log10(x):
+    try:
+        return Dual(np.log10(x.val), 1/(x.val * np.log(10)) * x.der)
+    except:
+        return np.log10(x)
+
+
--- code/pydif/pydif/pydif.py
+++ code/pydif/pydif/pydif.py
@@ -1,42 +1,54 @@
+"""pydif.py
+This file implements the autodiff class. The autodiff class is initialized with a function that accepts single variables of the form f(x,y,z,...).
+The autodiff object then allows for the evaluation of the value, and derivatives of a function at a specified position.
+
+Args:
+    param1 (function object): The function whose value and derivatives are to be evaluated.
+
+Returns:
+    autodiff Object: Returns a new autodiff object for evaluation of the specified function.
+
+"""
+
 from inspect import signature
+#from dual import dual
+from dualDEPRECATED import Dual
 import numpy as np
 import collections
-from dual import dual
 
 class autodiff():
 
+    #initialize autodiff class with a function
     def __init__(self, func):
         self.func = func
         self.num_params = len(signature(func).parameters)
 
+    #make function parameters dual numbers and evaluate function at a position
     def _eval(self, pos, jacobian = False):
-        #print(pos)
         if isinstance(pos, collections.Iterable):
             if jacobian:
                 params = []
                 for cursor, pos_i in enumerate(pos):
-                    der_clean = np.zeros(self.num_params)
-                    der_clean[cursor] = 1
-                    params.append(dual.Dual(pos_i, der_clean))
-                #print(params)
-                #print(*params)
-                return self.func(*params)
+                    der_partials = np.zeros(self.num_params)
+                    der_partials[cursor] = 1
+                    params.append(Dual(pos_i, der_partials))
             else:
-                params = [dual.Dual(pos_i,1) for pos_i in pos]
-                return self.func(*params)
+                params = [Dual(pos_i,1) for pos_i in pos]
+            return self.func(*params)
         else:
             if jacobian:
-                params = dual.Dual(pos,[1])
-                return self.func(params)
+                params = Dual(pos,[1])
             else:
-                params = dual.Dual(pos,1)
-                return self.func(params)                
+                params = Dual(pos,1)
+            return self.func(params)
 
+    #check that the specified position is the same shape as the function (specified at object creation) input
     def _check_pos(self, pos):
         if isinstance(pos, collections.Iterable):
             if len(pos) != self.num_params:
                 raise ValueError('poorly formatted position. should be of length {}.'.format(self.num_params))
 
+    #evaluate the value of the function at a specified position
     def get_val(self, pos):
         self._check_pos(pos)
         res = self._eval(pos)
@@ -44,11 +56,8 @@ class autodiff():
             return [i.val for i in res]
         else:
             return res.val
-        # if isinstance(pos, collections.Iterable):
-        #     return [i.val for i in self._eval(pos)]        
-        # else:
-        #     return self._eval(pos).val
 
+    #evaluate the derivatives of the function at a specified position
     def get_der(self, pos, jacobian = False, dir=None):
         if dir is None:
             self._check_pos(pos)
@@ -58,26 +67,10 @@ class autodiff():
                 return [i.der for i in res]
             else:
                 return res.der
-            # if jacobian:
-            #     if isinstance(res, collections.Iterable):
-            #         return [i.der for i in res]
-            #     else:
-            #         return res.der
-            # else:
-            #     if isinstance(res, collections.Iterable):
-            #         return [i.der for i in res]
-            #     else:
-            #         return res.der
-
-            # if isinstance(pos, collections.Iterable):
-            #     return [i.der for i in self._eval(pos)]        
-            # else:
-            #     return self._eval(pos).der
         else:
-            #seed vector
+            #seed vector/directional derivatives
             raise NotImplementedError
 
 
 if __name__ == '__main__':
-    func = lambda x: x+1
-    test = autodiff(func)
+    pass
