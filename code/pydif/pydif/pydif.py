3127b28737ec1a10b096fbaba7079bf6754a6f15
potato_farmer <WildTangles@gmail.com>
2018-11-08 12:09:54 -0500

    barebones autodiff class w newton test

--- code/pydif/pydif/pydif.py
+++ code/pydif/pydif/pydif.py
@@ -1,6 +1,7 @@
 from inspect import signature
+import numpy as np
 import collections
-from dual import Dual
+from dual import dual
 
 class autodiff():
 
@@ -8,12 +9,28 @@ class autodiff():
         self.func = func
         self.num_params = len(signature(func).parameters)
 
-    def _eval(self, pos):
+    def _eval(self, pos, jacobian = False):
+        #print(pos)
         if isinstance(pos, collections.Iterable):
-            params = [Dual(pos_i,1) for pos_i in pos]
+            if jacobian:
+                params = []
+                for cursor, pos_i in enumerate(pos):
+                    der_clean = np.zeros(self.num_params)
+                    der_clean[cursor] = 1
+                    params.append(dual.Dual(pos_i, der_clean))
+                #print(params)
+                #print(*params)
+                return self.func(*params)
+            else:
+                params = [dual.Dual(pos_i,1) for pos_i in pos]
+                return self.func(*params)
         else:
-            params = Dual(pos,1)
-        return self.func(params)
+            if jacobian:
+                params = dual.Dual(pos,[1])
+                return self.func(params)
+            else:
+                params = dual.Dual(pos,1)
+                return self.func(params)                
 
     def _check_pos(self, pos):
         if isinstance(pos, collections.Iterable):
@@ -22,18 +39,40 @@ class autodiff():
 
     def get_val(self, pos):
         self._check_pos(pos)
-        if isinstance(pos, collections.Iterable):
-            return [i.val for i in self._eval(pos)]        
+        res = self._eval(pos)
+        if isinstance(res, collections.Iterable):
+            return [i.val for i in res]
         else:
-            return self._eval(pos).val
+            return res.val
+        # if isinstance(pos, collections.Iterable):
+        #     return [i.val for i in self._eval(pos)]        
+        # else:
+        #     return self._eval(pos).val
 
-    def get_der(self, pos, dir=None):
+    def get_der(self, pos, jacobian = False, dir=None):
         if dir is None:
             self._check_pos(pos)
-            if isinstance(pos, collections.Iterable):
-                return [i.der for i in self._eval(pos)]        
+            res = self._eval(pos, jacobian)
+
+            if isinstance(res, collections.Iterable):
+                return [i.der for i in res]
             else:
-                return self._eval(pos).der
+                return res.der
+            # if jacobian:
+            #     if isinstance(res, collections.Iterable):
+            #         return [i.der for i in res]
+            #     else:
+            #         return res.der
+            # else:
+            #     if isinstance(res, collections.Iterable):
+            #         return [i.der for i in res]
+            #     else:
+            #         return res.der
+
+            # if isinstance(pos, collections.Iterable):
+            #     return [i.der for i in self._eval(pos)]        
+            # else:
+            #     return self._eval(pos).der
         else:
             #seed vector
             raise NotImplementedError
