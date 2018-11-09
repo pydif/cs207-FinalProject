233f171a9ee8b6df0315a8a760c92aca292666c9
potato_farmer <WildTangles@gmail.com>
2018-11-08 23:08:16 -0500

    movied newton example into top level

--- code/newton-example.py
+++ code/newton-example.py
@@ -1,4 +1,7 @@
 import numpy as np
+import sys
+import os
+sys.path.append(os.path.join(os.getcwd(),'pydif'))
 from pydif.pydif import autodiff
 from pydif.elementary import elementary as el
 
@@ -9,14 +12,13 @@ def f(x):
 dfdx = autodiff(f)
 
 # Start Newton algorithm
-xk = 0.1 # Initial guess
+xk = 1.0 # Initial guess
 tol = 1.0e-08 # Some tolerance
 max_it = 100 # Just stop if a root isn't found after 100 iterations
 
 root = None # Initialize root
 for k in range(max_it):
-    #delta_xk = -f(xk) / dfdx(xk) # Update Delta x_{k}
-    delta_xk = -f(xk) / dfdx.get_der(xk)
+    delta_xk = -f(xk) / dfdx.get_der(xk, jacobian=True)[0] #there is only 1 partial derivative that we care about
     if (abs(delta_xk) <= tol): # Stop iteration if solution found
         root = xk + delta_xk
         print("Found root at x = {0:17.16f} after {1} iteratons.".format(root, k+1))
