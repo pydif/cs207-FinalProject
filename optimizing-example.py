3d0f3737500a950ac9e68f4ae68c4a99e69b3a41
Victor Sheng <vysheng223@gmail.com>
2018-12-11 18:24:32 -0500

    update Optimize class name to capital O so example runs

--- optimizing-example.py
+++ optimizing-example.py
@@ -3,14 +3,14 @@ import sys
 import os
 sys.path.append(os.path.join(os.getcwd(),'code','pydif'))
 from pydif.pydif import autodiff
-from pydif.optimize.optimize import optimize
+from pydif.optimize.optimize import Optimize
 
 
 def f(x, y):
 	return x**2 + y**2
 
 
-x_opt = optimize(f)
+x_opt = Optimize(f)
 
 min_pos = x_opt.gradient_descent((7, 8))
 
