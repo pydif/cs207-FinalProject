0debf33436b3efa86c5864bff88585b11958f5fb
Hamish Nicholson <hamish_nicholson@college.harvard.edu>
2018-12-11 19:33:07 -0500

    dir structure

--- optimizing-example.py
+++ optimizing-example.py
@@ -3,15 +3,15 @@ import sys
 import os
 sys.path.append(os.path.join(os.getcwd(),'code','pydif'))
 from pydif.pydif import autodiff
-from pydif.optimize.optimize import optimize
+from pydif.optimize.optimize import Optimize
 
 
 def f(x, y):
 	return x**2 + y**2
 
 
-x_opt = optimize(f)
+x_opt = Optimize(f)
 
-min_pos = x_opt.gradient_descent((7, 8))
+min_pos = x_opt.gradient_descent((0, 10), step_size=0.01,max_iters=1000000)
 
 print(min_pos)
